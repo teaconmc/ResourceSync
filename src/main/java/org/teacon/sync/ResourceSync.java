5b70e448068229451d8a1b833d34a58be40f1600
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2021-06-13 12:02:40 +0800

    Add httpclient-cache dependency

--- src/main/java/org/teacon/sync/ResourceSync.java
+++ src/main/java/org/teacon/sync/ResourceSync.java
@@ -20,11 +20,13 @@
 
 package org.teacon.sync;
 
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.FilePack;
 import net.minecraft.resources.IPackFinder;
 import net.minecraft.resources.IPackNameDecorator;
 import net.minecraft.resources.ResourcePackInfo;
+import net.minecraft.util.LazyValue;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.DistExecutor;
@@ -32,54 +34,93 @@ import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.http.Header;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.cache.HttpCacheContext;
+import org.apache.http.client.cache.HttpCacheEntry;
+import org.apache.http.client.cache.HttpCacheStorage;
+import org.apache.http.client.cache.HttpCacheUpdateCallback;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.cache.CacheConfig;
+import org.apache.http.impl.client.cache.CachingHttpClients;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 
-import java.io.File;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.function.Consumer;
 
 @Mod("resource_sync")
-public class ResourceSync {
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public final class ResourceSync {
 
-    private static final Logger LOGGER = LogManager.getLogger("ResourceSync");
+    public static final Logger LOGGER = LogManager.getLogger("ResourceSync");
 
-    static ForgeConfigSpec.ConfigValue<String> packURL;
+    public static ForgeConfigSpec.ConfigValue<String> packURL;
 
     public ResourceSync() {
         ModLoadingContext context = ModLoadingContext.get();
         // Client-side only
-        context.registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (serverVer, isDedicated) -> true));
-        ForgeConfigSpec.Builder configSpecBuilder = new ForgeConfigSpec.Builder();
-        packURL = configSpecBuilder.comment("URL pointing to resource pack").define("packURL", "http://example.invalid");
-        context.registerConfig(ModConfig.Type.CLIENT, configSpecBuilder.build());
+        context.registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+                () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (serverVer, isDedicated) -> true));
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> Setup::setup);
     }
 
     static final class Setup {
 
-        static CompletableFuture<Void> downloadTask = CompletableFuture.completedFuture(null);
+        private static final LazyValue<HttpCacheStorage> storage = new LazyValue<>(() -> {
+            try {
+                Path dir = Minecraft.getInstance().gameDirectory.toPath().resolve("resource-sync");
+                Path cacheFile = Files.createDirectories(dir).resolve("cache.bin");
+                return new SingleFileCacheStorage(cacheFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
-        static void setup() {
-            final File gameDir = Minecraft.getInstance().gameDirectory;
-            final Path dst = gameDir.toPath().resolve("resourcepacks").resolve(".resource_sync.zip");
-            downloadTask = CompletableFuture.runAsync(new DownloadTask(dst));
-            Minecraft.getInstance().getResourcePackRepository().addPackFinder(new SyncedPackFinder(dst.toFile()));
+        private static final LazyValue<Path> dstPath = new LazyValue<>(() -> {
+            try {
+                Path dir = Minecraft.getInstance().gameDirectory.toPath().resolve("resource-sync");
+                return Files.createDirectories(dir).resolve("resources.zip");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        public static void setup() {
+            ForgeConfigSpec.Builder configSpecBuilder = new ForgeConfigSpec.Builder();
+
+            String comment = " URL pointing to resource pack";
+            packURL = configSpecBuilder.comment(comment).define("packURL", "http://example.invalid");
+            ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, configSpecBuilder.build());
+
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(Setup::setupConfig);
+        }
+
+        private static void setupConfig(ModConfig.Loading event) {
+            Minecraft.getInstance().getResourcePackRepository().addPackFinder(new SyncedPackFinder(dstPath.get()));
+        }
+
+        public static void launch() {
+            CompletableFuture.runAsync(new DownloadTask(storage.get(), dstPath.get())).join();
         }
     }
 
@@ -87,26 +128,32 @@ public class ResourceSync {
 
         private static final Marker PACK_FINDER_MARKER = MarkerManager.getMarker("PackFinder");
 
-        private final File searchTarget;
+        private final Path dstPath;
 
-        public SyncedPackFinder(File searchTarget) {
-            this.searchTarget = searchTarget;
+        public SyncedPackFinder(Path dstPath) {
+            this.dstPath = dstPath;
         }
 
         @Override
         public void loadPacks(Consumer<ResourcePackInfo> packInfoCallback, ResourcePackInfo.IFactory packInfoFactory) {
             try {
-                Setup.downloadTask.join();
+                Setup.launch();
+                packInfoCallback.accept(ResourcePackInfo.create(
+                        "resource_sync", true, () -> new ResourcePack(this.dstPath),
+                        packInfoFactory, ResourcePackInfo.Priority.TOP, IPackNameDecorator.BUILT_IN));
             } catch (Exception e) {
-                LOGGER.warn(PACK_FINDER_MARKER, "Uncaught occured while trying to fetch resource pack. ResourceSync will not try load any resource pack.", e);
-                return;
+                LOGGER.warn(PACK_FINDER_MARKER, "An error was thrown when finding the resource pack, ResourceSync will not try to load any resource pack.", e);
             }
-            if (this.searchTarget.exists() && this.searchTarget.isFile()) {
-                final ResourcePackInfo packInfo = ResourcePackInfo.create("ResourceSync", true, () -> new FilePack(this.searchTarget), 
-                        packInfoFactory, ResourcePackInfo.Priority.TOP, IPackNameDecorator.BUILT_IN);
-                packInfoCallback.accept(packInfo);
-            } else {
-                LOGGER.warn(PACK_FINDER_MARKER, "The synced resource pack is missing; ResourceSync will not try load any resource pack.");
+        }
+
+        private static final class ResourcePack extends FilePack {
+            public ResourcePack(Path target) {
+                super(target.toFile());
+            }
+
+            @Override
+            public String getName() {
+                return "Resource Sync";
             }
         }
     }
@@ -114,42 +161,123 @@ public class ResourceSync {
     static final class DownloadTask implements Runnable {
 
         private static final Marker DOWNLOAD_MARKER = MarkerManager.getMarker("Download");
+        private static final CacheConfig CONFIG = CacheConfig.custom()
+                .setMaxObjectSize(Integer.MAX_VALUE).setSharedCache(false).build();
 
-        private final Path dst;
+        private final Path dstPath;
+        private final String currentPackURL;
+        private final CloseableHttpClient client;
 
-        DownloadTask(Path dst) {
-            this.dst = dst;
+        DownloadTask(HttpCacheStorage cacheStorage, Path dstPath) {
+            this.dstPath = dstPath;
+            this.currentPackURL = packURL.get();
+            this.client = CachingHttpClients.custom().setCacheConfig(CONFIG).setHttpCacheStorage(cacheStorage).build();
         }
 
         @Override
         public void run() {
-            try {
-                URL url = new URL(packURL.get());
-                URLConnection conn = url.openConnection();
-                conn.setIfModifiedSince(System.currentTimeMillis());
-                if (conn instanceof HttpURLConnection) {
-                    ((HttpURLConnection) conn).setInstanceFollowRedirects(true);
+            HttpGet request = new HttpGet(this.currentPackURL);
+            HttpCacheContext context = HttpCacheContext.create();
+            try (CloseableHttpResponse response = this.client.execute(request, context)) {
+                LOGGER.debug(DOWNLOAD_MARKER, " >> {}", context.getRequest().getRequestLine());
+                for (Header header : context.getRequest().getAllHeaders()) {
+                    LOGGER.debug(DOWNLOAD_MARKER, " >> {}", header);
                 }
-                conn.connect();
-                if (conn instanceof HttpURLConnection) {
-                    if (((HttpURLConnection) conn).getResponseCode() == HttpURLConnection.HTTP_NOT_MODIFIED) {
-                        LOGGER.debug(DOWNLOAD_MARKER, "Remote server tells us that we can use our local copy (HTTP 304), abort downloading.");
-                        return;
-                    }
+                LOGGER.debug(DOWNLOAD_MARKER, " << {}", context.getResponse().getStatusLine());
+                for (Header header : context.getResponse().getAllHeaders()) {
+                    LOGGER.debug(DOWNLOAD_MARKER, " << {}", header);
                 }
-                final Path tempDst = Files.createTempFile("synced_pack-", ".zip");
-                try (ReadableByteChannel srcChannel = Channels.newChannel(conn.getInputStream())) {
-                    try (FileChannel dstChannel = FileChannel.open(tempDst)) {
-                        dstChannel.transferFrom(srcChannel, 0, Integer.MAX_VALUE);
-                        Files.move(tempDst, this.dst, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
-                    }
+                LOGGER.debug(DOWNLOAD_MARKER, "Remote server status: {}", context.getCacheResponseStatus());
+                try {
+                    Path tempDst = Files.createTempFile("synced-pack-", ".zip");
+                    response.getEntity().writeTo(Files.newOutputStream(tempDst));
+                    Files.move(tempDst, this.dstPath, StandardCopyOption.REPLACE_EXISTING);
+                    LOGGER.info(DOWNLOAD_MARKER, "Downloaded resource pack to {} from {}", tempDst, request.getURI());
                 } catch (IOException readError) {
                     LOGGER.warn(DOWNLOAD_MARKER, "Error occurred while downloading the resource pack from remote server. Download cannot continue.", readError);
+                    throw new CompletionException(readError);
                 }
-            } catch (MalformedURLException badURL) {
-                LOGGER.warn(DOWNLOAD_MARKER, "URL invalid, ResourceSync will not try download resource pack this time.");
+            } catch (ClientProtocolException protocolError) {
+                LOGGER.warn(DOWNLOAD_MARKER, "Detected invalid client protocol, ResourceSync will not try download resource pack this time.", protocolError);
+                throw new CompletionException(protocolError);
             } catch (IOException connError) {
                 LOGGER.warn(DOWNLOAD_MARKER, "Failed to establish connection, ResourceSync will not try download resource pack this time.", connError);
+                throw new CompletionException(connError);
+            }
+        }
+    }
+
+    public static final class SingleFileCacheStorage implements HttpCacheStorage {
+
+        private final Path path;
+        private volatile UUID key;
+
+        SingleFileCacheStorage(Path path) {
+            this.path = path;
+            try (ObjectInputStream stream = new ObjectInputStream(Files.newInputStream(path))) {
+                this.key = (UUID) stream.readObject();
+            } catch (ClassCastException | ClassNotFoundException | IOException e) {
+                this.key = new UUID(0L, 0L);
+            }
+        }
+
+        private void setEntry(UUID uuid, @Nullable HttpCacheEntry entry) throws IOException {
+            if (entry != null) {
+                try (ObjectOutputStream stream = new ObjectOutputStream(Files.newOutputStream(this.path))) {
+                    stream.writeObject(this.key = uuid);
+                    stream.writeObject(entry);
+                }
+            } else if (this.key.equals(uuid)) {
+                this.key = new UUID(0L, 0L);
+                Files.delete(this.path);
+            }
+        }
+
+        @Nullable
+        private HttpCacheEntry getEntry(UUID uuid) throws IOException {
+            if (this.key.equals(uuid)) {
+                try (ObjectInputStream stream = new ObjectInputStream(Files.newInputStream(this.path))) {
+                    Object object = stream.readObject();
+                    if (this.key.equals(object)) {
+                        return (HttpCacheEntry) stream.readObject();
+                    }
+                } catch (ClassCastException | ClassNotFoundException e) {
+                    throw new IOException(e);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        @Nullable
+        public HttpCacheEntry getEntry(String entryKey) throws IOException {
+            UUID uuid = UUID.nameUUIDFromBytes(entryKey.getBytes(StandardCharsets.UTF_8));
+            synchronized (this) {
+                return this.getEntry(uuid);
+            }
+        }
+
+        @Override
+        public void putEntry(String entryKey, HttpCacheEntry entry) throws IOException {
+            UUID uuid = UUID.nameUUIDFromBytes(entryKey.getBytes(StandardCharsets.UTF_8));
+            synchronized (this) {
+                this.setEntry(uuid, entry);
+            }
+        }
+
+        @Override
+        public void removeEntry(String entryKey) throws IOException {
+            UUID uuid = UUID.nameUUIDFromBytes(entryKey.getBytes(StandardCharsets.UTF_8));
+            synchronized (this) {
+                this.setEntry(uuid, null);
+            }
+        }
+
+        @Override
+        public void updateEntry(String entryKey, HttpCacheUpdateCallback callback) throws IOException {
+            UUID uuid = UUID.nameUUIDFromBytes(entryKey.getBytes(StandardCharsets.UTF_8));
+            synchronized (this) {
+                this.setEntry(uuid, callback.update(this.getEntry(uuid)));
             }
         }
     }
